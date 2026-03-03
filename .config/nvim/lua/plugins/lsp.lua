eb334175f779dabcf6d4732b02cb55ef22bd6372
Lucas Gutierrez <gutierrezjuanlucas@gmail.com>
2026-03-03 12:07:22 -0300

    neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,10 +5,22 @@ return {
     opts = {
       -- disable inlay hints globally
       inlay_hints = { enabled = false },
-
+      require("lspconfig").clangd.setup({
+        -- handlers = handlers,
+        capabilities = capabilities,
+        cmd = { "./clangd", "--background-index", "--compile-commands-dir=build", "--query-driver=*" },
+        root_dir = function()
+          -- leave empty to stop nvim from cd'ing into ~/ due to global .clangd file
+        end,
+      }),
       servers = {
         clangd = {
-          cmd = { "clangd" },
+          -- cmd = { "clangd", "--compile-commands-dir=build", "--query-driver=/path/to/xtensa-esp32-elf-gcc" },
+          cmd = {
+            "clangd",
+            "--compile-commands-dir=build",
+            "--query-driver=/home/lucas/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20250730/xtensa-esp-elf/bin/xtensa-esp32-elf-gcc",
+          },
           init_options = {
             inlayHints = { enabled = false },
           },
