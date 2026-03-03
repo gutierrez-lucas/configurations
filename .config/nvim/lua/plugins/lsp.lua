c0214ab9d242b6ada630967b223c64b0a9e46a3d
Lucas Gutierrez <gutierrezjuanlucas@gmail.com>
2026-03-03 11:03:26 -0300

    neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,1 +1,22 @@
-return { { "neovim/nvim-lspconfig", opts = { servers = { clangd = {}, pyright = {}, gopls = {}, tsserver = {} } } } }
+-- ~/.config/nvim/lua/plugins/lsp.lua
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- disable inlay hints globally
+      inlay_hints = { enabled = false },
+
+      servers = {
+        clangd = {
+          cmd = { "clangd" },
+          init_options = {
+            inlayHints = { enabled = false },
+          },
+        },
+        pyright = {},
+        gopls = {},
+        tsserver = {},
+      },
+    },
+  },
+}
